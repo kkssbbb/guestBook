bd250b9043c00488517adadae99c838275e391a6
KING_MASS <03337@naver.com>
2022-07-03 17:58:50 +0900

    간단한 crud방명록

--- src/main/java/com/crud/guestbook/controller/GuestBookController.java
+++ src/main/java/com/crud/guestbook/controller/GuestBookController.java
@@ -1,2 +1,19 @@
-package com.crud.guestbook.controller;public class GuestBookController {
+package com.crud.guestbook.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/guestbook")
+public class GuestBookController {
+
+    @GetMapping({"/","/list"})
+    public String list(){
+        log.info("리스트메서드 호출..");
+        return "/guestbook/list";
+    }
 }
